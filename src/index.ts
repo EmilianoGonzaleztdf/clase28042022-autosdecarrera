902c57bd1ca88d6f6918fc970363c9d22d63f821
Emiliano Nicolas Gonzalez <103612852+EmilianoGonzaleztdf@users.noreply.github.com>
2022-05-18 15:55:41 -0300

    funciona =) hice el ejercicio. Tarde pero seguro.

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,16 @@ rotulo1.innerHTML = "ingrese la vuelta 1";
 rotulo2.innerHTML = "ingrese la vuelta 2";
 rotulo3.innerHTML = "ingrese la vuelta 3";
 rotulo4.innerHTML = "ingrese la vuelta 4";
-/* defino las varibles y las convierto a num */
-let vuelta1: number = Number(dato1.value);
-let vuelta2: number = Number(dato2.value);
-let vuelta3: number = Number(dato3.value);
-let vuelta4: number = Number(dato4.value);
+
 /* genero accion del boton*/
 btn1.addEventListener("click", () => {
+  /* defino las varibles y las convierto a num */
+  let vuelta1: number = Number(dato1.value);
+  let vuelta2: number = Number(dato2.value);
+  let vuelta3: number = Number(dato3.value);
+  let vuelta4: number = Number(dato4.value);
+
+  console.log("TEST BUTTON"); //para testear que el boton funciona
   let tiempoTotal: number = vuelta1 + vuelta2 + vuelta3 + vuelta4;
   let promedioDeVuelta: number = tiempoTotal / 4;
   console.log("Tiempo total: ", tiempoTotal);
