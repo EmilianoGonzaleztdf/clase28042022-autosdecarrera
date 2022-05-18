bde45c5f05dc3da16a01717b6c6d2cd0c9c4edde
Emiliano Nicolas Gonzalez <103612852+EmilianoGonzaleztdf@users.noreply.github.com>
2022-05-18 15:53:31 -0300

    genere el codigo deveria funcionar (error in sandbox) commit y a probar de nuevo.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
-import "./styles.css";
+/* llamo a los elementos del HTML*/
+let dato1 = document.getElementById("dato1");
+let dato2 = document.getElementById("dato2");
+let dato3 = document.getElementById("dato3");
+let dato4 = document.getElementById("dato4");
+let btn1 = document.getElementById("btn1");
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+/* inserto los rotulos via innerHTML */
+rotulo1.innerHTML = "ingrese la vuelta 1";
+rotulo2.innerHTML = "ingrese la vuelta 2";
+rotulo3.innerHTML = "ingrese la vuelta 3";
+rotulo4.innerHTML = "ingrese la vuelta 4";
+/* defino las varibles y las convierto a num */
+let vuelta1: number = Number(dato1.value);
+let vuelta2: number = Number(dato2.value);
+let vuelta3: number = Number(dato3.value);
+let vuelta4: number = Number(dato4.value);
+/* genero accion del boton*/
+btn1.addEventListener("click", () => {
+  let tiempoTotal: number = vuelta1 + vuelta2 + vuelta3 + vuelta4;
+  let promedioDeVuelta: number = tiempoTotal / 4;
+  console.log("Tiempo total: ", tiempoTotal);
+  console.log("promedio de vuelta: ", promedioDeVuelta);
+});
